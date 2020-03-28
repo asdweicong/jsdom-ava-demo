0d58d9b6c74d8fdfc1d5f4422d710d6094d08602
weicon <1418066959@qq.com>
2020-03-29 00:59:41 +0800

    ad

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # jsdom-ava-demo
-jsdom-ava-demo
+
+
+首先安装 jsdom，这倒是很简单：
+
+$ npm install --save jsdom
+接着安装 ava，最好先全局安装一遍：
+
+$ npm install -g ava
+
+$ npm install --save ava
+然后为了方便使用 npm test 命令，执行下面的命令：
+
+$ ava --init
+
+##配置  https://segmentfault.com/a/1190000005834279
+
+```bash
+
+$ npm install
+
+# 测试串行
+$ ava -v ./serial.js
+
+# 测试并行
+$ ava -v ./parallel.js
+
+```
---
+++ parallel.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+import { jsdom } from 'jsdom';
+
+import test from 'ava';
+
+var window = jsdom(fs.readFileSync('./test.html')).defaultView;
+var document = window.document;
+
+function jsdomTest() {
+    return new Promise(function (resolve, reject) { 
+        jsdom.jQueryify(window, "http://apps.bdimg.com/libs/jquery/2.1.4/jquery.js", function () {
+            resolve(window.jQuery);
+        });
+    });
+}
+
+test('点击测试', async t => {
+    var $ = await jsdomTest();
+
+    var $div = $('#div');
+
+    var colorBeforeClick, colorAfterClick;
+
+    console.log(colorBeforeClick = $div.css('background-color'));
+
+    $div.trigger('click');
+
+    console.log(colorAfterClick = $div.css('background-color'));
+   
+    t.not(colorBeforeClick, colorAfterClick, 'bgColor changed');
+});
+
+
+test('串行测试', function (t) {
+    console.log(window.$)
+    t.falsy(window.$, '串行失败');
+});
